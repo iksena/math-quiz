1a839d4288d8b45d7e8622749ef2f62e26021b77
I Komang Sena <i.buwana@btpn.com>
2020-04-25 11:42:07 +0700

    added markdown component

---
+++ src/Components/Markdown.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
+import RemarkMathPlugin from 'remark-math';
+
+const _mapProps = (props) => ({
+  ...props,
+  plugins: [
+    RemarkMathPlugin,
+  ],
+  renderers: {
+    ...props.renderers,
+    math: (opts) => <BlockMath math={opts.value} />,
+    inlineMath: (opts) => <InlineMath math={opts.value} />,
+  },
+});
+
+const Markdown = (props) => (
+  <ReactMarkdown {..._mapProps(props)} />
+);
+
+export default Markdown;
--- src/Components/Question.component.js
+++ src/Components/Question.component.js
@@ -1,36 +1,19 @@
 import React from 'react';
-import Latex from 'react-latex';
 import PropTypes from 'prop-types';
+import Markdown from './Markdown.component';
 
 const _renderContent = (content) => (
-  <Latex>{content}</Latex>
+  <Markdown>{content}</Markdown>
 );
 
-const _renderImage = (image, index) => (
-  <>
-    <h4>
-      Image
-      {index + 1}
-    </h4>
-    <br />
-    <img src={image} alt={`image ${index + 1}`} />
-  </>
-);
-
-const _renderImages = (images) => images.map(_renderImage);
-
-const Question = ({ content, images }) => (
+const Question = ({ content }) => (
   <>
     {_renderContent(content)}
-    {_renderImages(images)}
   </>
 );
 
 Question.propTypes = {
   content: PropTypes.string.isRequired,
-  images: PropTypes.arrayOf(PropTypes.shape({
-    image: PropTypes.string,
-  })).isRequired,
 };
 
 export default Question;
